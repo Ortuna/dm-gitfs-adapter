a11adb21e3aa7b29c76a97fb9f3e55ce3744cf94
Sumeet Singh <ortuna@gmail.com>
2013-05-20 19:12:38 -0400

    Don't know what I was doing there

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,3 @@ root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $:.unshift root
 
 SPEC_PATH    = File.expand_path(File.dirname(__FILE__))
-FIXTURE_PATH = "#{SPEC_PATH}/fixtures/**/*.rb"
-
-Dir[FIXTURE_PATH].each{ |fixture| require fixture }
