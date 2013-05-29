3c8f75de24cf3b0ce32bf1639027bda7983666f3
Sumeet Singh <ortuna@gmail.com>
2013-05-28 22:05:52 -0400

    Extracted methods to less complex ones

--- lib/dm-gitfs-adapter/adapter_extensions/file.rb
+++ lib/dm-gitfs-adapter/adapter_extensions/file.rb
@@ -4,18 +4,27 @@ module DataMapper
 
       private
       def read_file(query)
-        records   = []
         params    = extract_query_params(query)
         root_path = params[:root_path] || @path
-        Dir.glob("#{root_path}/**/*").each do |item|
-          next if !::File.file?(item) || ::File.basename(item)[0] == '_'
-          record  = query.model.new
-          record.send(:path=, item)
-          record.send(:base_path=, ::File.basename(item))
-          records << record
-        end
+        records   = parse_directory_files(query.model, "#{root_path}/**/*")
         set_parent_model(records, params) if params[:path_key]
         records
+      end
+
+      def parse_directory_files(model, path)
+        records = []
+        Dir.glob(path).each do |item|
+          next if !::File.file?(item) || ::File.basename(item)[0] == '_'
+          records << init_object_from_path(model, item)
+        end
+        records
+      end
+
+      def init_object_from_path(model, item)
+        model.new.tap do |record|
+          record.send(:path=, item)
+          record.send(:base_path=, ::File.basename(item))
+        end
       end
 
       def set_parent_model(records, params)
